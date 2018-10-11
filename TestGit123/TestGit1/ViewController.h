53095780de8c90eb49bee896121beac0311e6e02
秦光义 <qinguangyi@moonfintech.com>
2018-10-11 14:40:51 +0800

    提交信息1

---
+++ TestGit123/TestGit1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGit1
+//
+//  Created by Kevin on 2018/10/10.
+//  Copyright © 2018年 Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestGit123/TestGit1/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestGit1
+//
+//  Created by Kevin on 2018/10/10.
+//  Copyright © 2018年 Kevin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
